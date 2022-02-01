d1ac8674b630fccc2b206d9e7be7b42b0e2099c6
Onizukafr <92436288+Onizukafr@users.noreply.github.com>
2022-02-01 17:02:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ import pandas as pd
 import streamlit as st
 import os
 
-with st.file_input() as input:
-  if input == None:
-    st.warning('No file selected.')
-  else:
-    file_contents = input.read()
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.getvalue()
+     st.write(bytes_data)
 
-list_keywords = pd.read_excel(filename) 
+     # To convert to a string based IO:
+     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+     st.write(stringio)
+
+     # To read file as string:
+     string_data = stringio.read()
+     st.write(string_data)
+
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
+
+list_keywords = pd.read_excel(uploaded_file) 
 list_keywords = list_keywords.values.tolist()
 
 list_urls = []
